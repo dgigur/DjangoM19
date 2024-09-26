962b7b45b3f12602155aa76a40acc267f28993e3
dgigur <dgigurda005@mail.ru>
2024-09-26 21:01:28 +0300

    Урок 1

--- M19/task1/models.py
+++ M19/task1/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Buyer(models.Model):
     name = models.CharField(max_length=50)
-    balance = models.DecimalField(decimal_places=2, max_digits=8)
+    balance = models.DecimalField(decimal_places=2, max_digits=7)
     age = models.IntegerField()
 
 
 class Game(models.Model):
     title = models.CharField(max_length=50)
-    cost = models.DecimalField(decimal_places=2, max_digits=8)
-    size = models.DecimalField(decimal_places=2, max_digits=8)
+    cost = models.DecimalField(decimal_places=2, max_digits=7)
+    size = models.DecimalField(decimal_places=2, max_digits=7)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer)
